5c1c872589dde2eb88dc9ded6b88df8db520c426
Louis Dorard <louis@louisdorard.com>
2019-07-14 16:30:56 +0100

    Remove RISE instructions
    Not needed since new version of RISE (pip install enough)

--- init-workspace.sh
+++ init-workspace.sh
@@ -1,4 +1,2 @@
 # mkdir -p ~/.ssh
 # echo "Host gitlab.com\nIdentityFile /floyd/input/ssh/ml" > ~/.ssh/config
-jupyter-nbextension install rise --py --sys-prefix
-jupyter-nbextension enable rise --py --sys-prefix
